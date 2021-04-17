46ad5e47360f2beca948a419b99b66f531513e24
Mike <mike@mikera.net>
2021-04-17 12:09:31 +0800

    Add JUnit 5 testing

---
+++ src/test/java/blockgame/BlockTest.java
@@ -0,0 +1,12 @@
+package blockgame;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class BlockTest {
+
+	@Test public void test1() {
+		assertTrue(true);
+	}
+}
