980264b29147f0e446826268e37faba659c0118d
Mike <mike@mikera.net>
2021-09-11 13:50:57 +0800

    Add resource for textures

---
+++ src/main/java/blockgame/assets/Assets.java
@@ -0,0 +1,23 @@
+package blockgame.assets;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Assets {
+
+	public static BufferedImage spriteImage;
+
+	static {
+		//ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+		ClassLoader classLoader =Assets.class.getClassLoader();
+
+		try {
+			Assets.spriteImage = ImageIO.read(classLoader.getResource("images/textures.png"));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
--- src/test/java/blockgame/BlockTest.java
+++ src/test/java/blockgame/BlockTest.java
@@ -1,12 +1,22 @@
 package blockgame;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import blockgame.assets.Assets;
 
 public class BlockTest {
 
 	@Test public void test1() {
 		assertTrue(true);
+	}
+	
+	@Test
+	
+	public void testAssets() {
+		assertEquals(2048,Assets.spriteImage.getWidth());
+		assertEquals(2048,Assets.spriteImage.getHeight());
 	}
 }
